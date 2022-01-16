426126a752c625b092c9cdd1256c78fe6af0341f
skiyl9x <skiyl9x@gmail.com>
2022-01-16 05:53:00 +0200

    fix: new file .env_examples & change vars in docker-compose.yml

--- build.sh
+++ build.sh
@@ -13,11 +13,12 @@ echo "* generate db passwords"
 root_pass=`date +%s | sha256sum | base64 | head -c 32 ; echo` #root mysql pass
 db_pass=`date +%s | sha256sum | base64 | head -c 32 ; echo` #wordpress db pass
 
+cp .env_examples .env
 echo "* generate .env"
 echo -e "ROOT_PASS=$root_pass
 DB_NAME=wordpress
 DB_USER=user
-DB_PASS=$db_pass" > .env
+DB_PASS=$db_pass" >> .env
 
 echo "* replace wp-config.php data"
 cp $site_dir/wp-config-sample.php $site_dir/wp-config.php
@@ -28,8 +29,6 @@ sed -i "s/localhost/db/" $site_dir/wp-config.php
 echo "* deploy docker-compose.yml" 
 docker-compose up -d;
 
-echo "* import wordpress.sql to wordpress db"
-container_id=`docker ps --filter "expose=3306" --format "table {{.ID}}" | tail -1`
 echo "* waiting 10 sec"
 sleep 10
 echo "* importing db"
--- data/site/wp-config.php
+++ data/site/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 'database_name_here' );
 
 /** MySQL database username */
-define( 'DB_USER', 'user' );
+define( 'DB_USER', 'username_here' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'ZDFiNTEwNjJkMmNlZWEyZDI2ZTkwMTJj' );
+define( 'DB_PASSWORD', 'password_here' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'db' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
