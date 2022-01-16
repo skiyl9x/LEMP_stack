90394dfe294aeeeedf429e94ec57ba432053d882
skiyl9x <skiyl9x@gmail.com>
2022-01-16 04:27:44 +0200

    change site dir

--- build.sh
+++ build.sh
@@ -8,23 +8,23 @@ function error {
   fi;
 }
 
-
+site_dir="./data/site"
 echo "* generate db passwords"
 root_pass=`date +%s | sha256sum | base64 | head -c 32 ; echo` #root mysql pass
 db_pass=`date +%s | sha256sum | base64 | head -c 32 ; echo` #wordpress db pass
 
 echo "* generate .env"
-echo -e "ROOT_PASS=$root_pass
+echo -e "ROOT_PASS=${root_pass}
 DB_NAME=wordpress
 DB_USER=user
-DB_PASS=$db_pass" > .env
+DB_PASS=${db_pass}" > .env
 
 echo "* replace wp-config.php data"
-cp ./data/site_wp/wp-config-sample.php ./data/site_wp/wp-config.php; error $? "can't copy file"
-sed -i "s/database_name_here/wordpress/" ./data/site_wp/wp-config.php; error $? "can't replase word"
-sed -i "s/username_here/user/" ./data/site_wp/wp-config.php $? "can't replase word"
-sed -i "s/password_here/$db_pass/" ./data/site_wp/wp-config.php $? "can't replase word"
-sed -i "s/localhost/db/" ./data/site_wp/wp-config.php $? "can't replase word"
+cp ${site_dir}/wp-config-sample.php ${site_dir}/wp-config.php; error $? "can't copy file"
+sed -i "s/database_name_here/wordpress/" ${site_dir}/wp-config.php; error $? "can't replase word"
+sed -i "s/username_here/user/" ${site_dir}/wp-config.php $? "can't replase word"
+sed -i "s/password_here/${db_pass}/" ${site_dir}/wp-config.php $? "can't replase word"
+sed -i "s/localhost/db/" ${site_dir}/wp-config.php $? "can't replase word"
 echo "* deploy docker-compose.yml" 
 docker-compose up -d
 
@@ -33,4 +33,4 @@ container_id=`docker ps --filter "expose=3306" --format "table {{.ID}}" | tail -
 echo "* waiting 10 sec"
 sleep 10
 echo "* importing db"
-docker exec -i lemp_stack_pipeline_db_1   mysql -uuser -p$db_pass wordpress < wordpress.sql
+docker exec -i lemp_stack_pipeline_db_1   mysql -uuser -p${db_pass} wordpress < wordpress.sql
