4dc21a1dd3b2b5ed4c535c7b75a342a806272e25
skiyl9x <skiyl9x@gmail.com>
2022-01-17 16:29:43 +0200

    fix: add post test function in test.sh

--- test.sh
+++ test.sh
@@ -5,18 +5,30 @@ function error {
   exit 1;
 }
 
+function clear_all {
+  echo "* stop all containers"
+  docker-compose down
+  source .env_examples
+  export NGINX_IMAGE PHP_IMAGE MARIADB_IMAGE
+  echo "* delete all images"
+  docker rmi $NGINX_IMAGE $PHP_IMAGE $MARIADB_IMAGE
+}
+
+
+
+
 echo "* start testing"
 
 echo -e "\n* check running containers"
 number_up=`docker-compose ps | grep Up | wc -l`
 docker-compose ps
-if [ $number_up -ne "3" ]; then 
-  
-  error "some containers is down"; 
+if [ $number_up -ne "3" ]; then
+
+  error "some containers is down";
 
 fi
 echo -e "\n* checking http status"
 curl -I http://my-registry.pp.ua
 [[ `curl -Is http://my-registry.pp.ua | grep '200 OK'` ]] || error "bad http status";
 
-docker-compose down
+clear_all
