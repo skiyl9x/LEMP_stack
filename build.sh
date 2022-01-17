7371050b113cc23d041e6f0bf9dd5c090be23f57
skiyl9x <skiyl9x@gmail.com>
2022-01-17 16:59:16 +0200

    feature: build.sh messages have divided by enter

--- build.sh
+++ build.sh
@@ -14,22 +14,22 @@ echo "* generate db passwords"
 root_pass=`date +%s | sha256sum | base64 | head -c 32 ; echo` #root mysql pass
 db_pass=`date +%s | sha256sum | base64 | head -c 32 ; echo` #wordpress db pass
 
-echo "* generate .env"
+echo -e "\n* generate .env"
 cp .env_examples .env
 echo -e "ROOT_PASS=$root_pass
 DB_NAME=wordpress
 DB_USER=user
 DB_PASS=$db_pass" >> .env
 
-echo "* replace wp-config.php data"
+echo -e "\n* replace wp-config.php data"
 cp $site_dir/wp-config-sample.php $site_dir/wp-config.php
 sed -i "s/database_name_here/wordpress/; s/username_here/user/; s/password_here/$db_pass/; s/localhost/db/" $site_dir/wp-config.php
 
-echo "* deploy docker-compose.yml" 
+echo -e "\n* deploy docker-compose.yml" 
 docker-compose up -d;
 
-echo "* waiting 10 sec"
+echo -e "\n* waiting 10 sec"
 sleep 10
 
-echo "* importing db"
+echo -e "\n* importing db"
 docker exec -i lemp_stack_pipeline_db_1   mysql -uuser -p$db_pass wordpress < data/wordpress.sql
