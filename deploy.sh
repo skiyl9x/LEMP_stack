ae73fca36fdadbf7feb76d22d39a158e27f305e1
skiyl9x <skiyl9x@gmail.com>
2022-01-18 23:32:24 +0200

    fix: mistakes in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,17 +15,17 @@ root_pass=`date +%s | sha256sum | base64 | head -c 32 ; echo` #root mysql pass
 db_pass=`date +%s | sha256sum | base64 | head -c 32 ; echo` #wordpress db pass
 
 echo -e "\n* generate .env"
-cp .env_examples .env; "no such file .env_examples"
+cp .env_examples .env; error $? "no such file .env_examples"
 echo -e "ROOT_PASS=$root_pass
 DB_NAME=wordpress
 DB_USER=user
-DB_PASS=$db_pass" >> .env; error $?
+DB_PASS=$db_pass" >> .env; 
 
 echo -e "\n* replace wp-config.php data"
-cp $site_dir/wp-config-sample.php $site_dir/wp-config.php; error $? "no such file wp-conig-sample.php"
-sed -i "s/database_name_here/wordpress/; s/username_here/user/; s/password_here/$db_pass/; s/localhost/db/" $site_dir/wp-config.php error $? "can't replace this data in wp-config.php"
+cp $site_dir/wp-config-sample.php $site_dir/wp-config.php; error $? "no such file wp-conig-sample.php" 
+sed -i "s/database_name_here/wordpress/; s/username_here/user/; s/password_here/$db_pass/; s/localhost/db/" $site_dir/wp-config.php; error $? "can't replace this data in wp-config.php"
 
-echo -e "\n* deploy docker-compose.yml" 
+echo -e "\n* deploy docker-compose.yml"
 docker-compose up -d; error $? "error with deploy apps"
 
 echo -e "\n* waiting 10 sec"
