1c066e8fe20a53d970910a9010c780065d84a308
skiyl9x <skiyl9x@gmail.com>
2022-01-20 22:59:09 +0200

    fix: add helm repo add

--- prometheus_and_grafana.sh
+++ prometheus_and_grafana.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "* installing prometheus and grafana into kubernetes cluster"
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
 helm install prometheus prometheus-community/kube-prometheus-stack
- kubectl port-forward deploy/prometheus-grafana 3000
+kubectl port-forward deploy/prometheus-grafana 3000
 
